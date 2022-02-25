2863ede534b9655913e2fd9eec31f88f53e1b28e
WafaBergaoui <wafabergaoui3@gmail.com>
2022-02-25 11:34:59 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import React from "react";
+import SearchBox from "./components/SearchBox";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <p>React APP</p>
+    <div>
+      <Router>
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <div>
+                <img className="Bitmap" src="./assets/logo.jpg" alt="logo" />
+                <SearchBox />
+              </div>
+            </div>
+          </header>
+        </div>
+        <main>
+          
+        </main>
+      </Router>
     </div>
   );
 }
